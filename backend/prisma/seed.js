fd6086d9dc57024613f2ab3695ec1cc37ef81c59
PacifistRam <141394720+PacifistRam@users.noreply.github.com>
2024-10-28 07:09:47 +0000

    changed env content

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -5,7 +5,7 @@ const { faker } = require("@faker-js/faker");
 //  function createRandomUser() {
 //   return {
 //     email: faker.internet.email(),
-//     userName: faker.internet.userName(),
+//     userName: faker.internet.username(),
 //     password: faker.internet.password(),
 //   };
 // }
@@ -50,68 +50,70 @@ const bios = faker.helpers.multiple(createRandomBio, {
 
 const profiles = [
   {
-    firstName: "zetta",
+    firstName: "Mikel",
+    lastName: "Kulas",
     bio: bios[0],
     profilePic: avatars[0],
     userId: 1,
   },
   {
-    firstName: "Kristofer ",
-    lastName: "Ziemann",
+    firstName: "Valentina",
+    lastName: "Sauer",
     bio: bios[1],
     profilePic: avatars[1],
     userId: 2,
   },
   {
-    firstName: "Samara",
-    lastName: "Erdman",
+    firstName: "Marguerite",
+    lastName: "Rolfson",
     bio: bios[2],
     profilePic: avatars[2],
     userId: 3,
   },
   {
-    firstName: "Princess",
-    lastName: "Stokes",
+    firstName: "Ima",
+    lastName: "Graham",
     bio: bios[3],
     profilePic: avatars[3],
     userId: 4,
   },
   {
-    firstName: "Haylee",
+    firstName: "Petra",
+    lastName: "Wisoky",
     bio: bios[4],
     profilePic: avatars[4],
     userId: 5,
   },
   {
-    firstName: "Norma",
-    lastName: "Pollich",
+    firstName: "Lionel",
     bio: bios[5],
     profilePic: avatars[5],
     userId: 6,
   },
   {
-    firstName: "Lila",
+    firstName: "Davon",
+    lastName: "Wunsch",
     bio: bios[6],
     profilePic: avatars[6],
     userId: 7,
   },
   {
-    firstName: "Carole",
-    lastName: "Marvin",
+    firstName: "Marjorie",
+    lastName: "Hickle86",
     bio: bios[7],
     profilePic: avatars[7],
     userId: 8,
   },
   {
-    firstName: "Jared",
-    lastName: "Barton",
+    firstName: "Merritt",
+    lastName: "Kihn",
     bio: bios[8],
     profilePic: avatars[8],
     userId: 9,
   },
   {
-    firstName: "Jaeden",
-    lastName: "Gulgowski",
+    firstName: "Jany",
+    lastName: "Spencer",
     bio: bios[9],
     profilePic: avatars[9],
     userId: 10,
@@ -127,21 +129,21 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function main() {
-//     // const addUsers = await prisma.user.createMany({
-//     //     data: users,
-//     //     skipDuplicates: true
-//     // })
-//     // console.log("addUsers :", addUsers)
+    // const addUsers = await prisma.user.createMany({
+    //     data: users,
+    //     skipDuplicates: true
+    // })
+    // console.log("addUsers :", addUsers)
 
-//     const addPosts = await prisma.post.createMany({
-//         data: posts,
-//         skipDuplicates: true
-//     })
+    // const addPosts = await prisma.post.createMany({
+    //     data: posts,
+    //     skipDuplicates: true
+    // })
     const addProfile = await prisma.profile.createMany({
         data: profiles,
         skipDuplicates: true
     })
-    console.log(addProfile)
+    // console.log(addProfile)
 }
 
 main()
