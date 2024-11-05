69e64a78b43eff753368a5fb3fb84383d64bcb1a
PacifistRam <141394720+PacifistRam@users.noreply.github.com>
2024-11-05 06:19:27 +0000

    added sign-up

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,36 +1,83 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
-import 
+
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const loginFormSchema = z.object({
+  email: z.string().email({
+    message: "Input a valid email",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters",
+  }),
+});
+
+type loginFormSchema = z.infer<typeof loginFormSchema>
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<loginFormSchema>({
+    resolver: zodResolver(loginFormSchema),
+  });
+
+  const onSubmit = async (data: loginFormSchema) => {
+    console.log(data);
+    reset();
+    // Handle your login logic here
+  };
+
   return (
-    <div className="h-full w-full flex fle-col justify-center items-center">
+    <div className="h-full w-full flex flex-col justify-center items-center">
       <section className="bg-accent px-4 py-6 rounded-lg mt-2 w-full max-w-sm space-y-4">
-        <h1 className="text-xl text-center font-bold leading-tight tracking-tight">Sign in to your account</h1>
+        <h1 className="text-xl text-center font-bold leading-tight tracking-tight">
+          Sign in to your account
+        </h1>
         <div className="space-y-4">
-          <form className="space-y-4 md:space-y-6 " >
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 md:space-y-6">
             <div className="grid w-full max-w-sm items-center gap-1.5">
               <Label htmlFor="email">Email</Label>
-              <Input type="email" placeholder="example@mail.com" />
+              <Input
+                {...register("email")}
+                type="email"
+                placeholder="example@mail.com"
+              />
+              {errors.email && <p className="text-red-500">{errors.email.message}</p>}
             </div>
             <div className="grid w-full max-w-sm items-center gap-1.5">
               <Label htmlFor="password">Password</Label>
-              <Input type="password" placeholder="min 6 characters" />
+              <Input
+                {...register("password")}
+                type="password"
+                placeholder="min 6 characters"
+              />
+              {errors.password && <p className="text-red-500">{errors.password.message}</p>}
             </div>
-            <Button type="submit" className="w-full max-w-sm font-semibold text-lg text-foreground" >Log-in </Button>
+            <Button
+              type="submit"
+              className="w-full max-w-sm font-semibold text-lg text-foreground"
+              disabled={isSubmitting} // Disable button while submitting
+            >
+              Log in
+            </Button>
           </form>
           <div className="flex items-center gap-1 px-2">
-            <div className=" flex-grow border-b border-muted-foreground " ></div>
+            <div className="flex-grow border-b border-muted-foreground"></div>
             <span> OR</span>
-            <div className="flex-grow border-b  border-muted-foreground"></div>
+            <div className="flex-grow border-b border-muted-foreground"></div>
           </div>
-          
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,9 +1,82 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
+const signUpSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+type signUpSchema = z.infer<typeof signUpSchema>
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<signUpSchema>({
+    resolver: zodResolver(signUpSchema),
+  });
 
-export default SignUp
+  const onSubmit = async (data: signUpSchema) => {
+    console.log(data);
+    reset();
+    // Handle your login logic here
+  };
+
+  return (
+    <div className="h-full w-full flex flex-col justify-center items-center">
+      <section className="bg-accent px-4 py-6 rounded-lg mt-2 w-full max-w-sm space-y-4">
+        <h1 className="text-xl text-center font-bold leading-tight tracking-tight">
+          {" "}
+          Create An Account
+        </h1>
+        <div className="space-y-4">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="space-y-4 md:space-y-6"
+          >
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                {...register("email")}
+                type="email"
+                placeholder="example@mail.com"
+              />
+              {errors.email && (
+                <p className="text-red-500">{errors.email.message}</p>
+              )}
+            </div>
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                {...register("password")}
+                type="password"
+                placeholder="min 6 characters"
+              />
+              {errors.password && (
+                <p className="text-red-500">{errors.password.message}</p>
+              )}
+            </div>
+            <Button
+              type="submit"
+              className="w-full max-w-sm font-semibold text-lg text-foreground"
+              disabled={isSubmitting} // Disable button while submitting
+            >
+              Log in
+            </Button>
+          </form>
+          <div className="flex items-center gap-1 px-2">
+            <div className="flex-grow border-b border-muted-foreground"></div>
+            <span> OR</span>
+            <div className="flex-grow border-b border-muted-foreground"></div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default SignUp;
